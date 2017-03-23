f2294c1786c646d4fe87d46bd60690728c6c643a
AlanLeszczuk <alan.leszczuk@altimi.com>
2017-03-23 18:13:21 +0100

    change favicon, implement initial routing for first two views from the Wireframe

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,14 @@
+import {Router, RouterConfiguration} from 'aurelia-router';
+
 export class App {
-  message = 'Hello World!';
+  router: Router;
+
+  configureRouter(config: RouterConfiguration, router: Router) {
+    config.title = 'HMS';
+    config.map([
+      {route: ['', 'login'], moduleId:'login-view/login-view', title: 'Login'},
+      {route: 'base-view', moduleId:'client/base-view/base-view', title: 'Hello Guest!'}
+    ]);
+    this.router = router;
+  }
 }
