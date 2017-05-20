2a7ab9bdd0df2cb95593a109286bd07dd26520db
Salarenko <218522@student.pwr.edu.pl>
2017-05-20 16:42:52 +0200

    Added return buttons + redirection logic

--- src/views/client/cases/room-service/room-service.ts
+++ src/views/client/cases/room-service/room-service.ts
@@ -1,13 +1,14 @@
 import {inject} from 'aurelia-framework';
+import {Router, RouterConfiguration} from 'aurelia-router';
 import {CasesService} from '../../../../services/cases-service';
 
 
-@inject(CasesService)
+@inject(CasesService, Router)
 export class RoomService {
   cases: {}[];
   selectedCase: {};
 
-  constructor(private casesService: CasesService) {
+  constructor(private casesService: CasesService, private router: Router) {
     casesService.getCleanerCases().then(res => {
       let tmpCases = JSON.parse(JSON.stringify(res));
       this.cases = tmpCases;
@@ -18,5 +19,9 @@ export class RoomService {
   selectCase(selectedCase) {
     this.selectedCase = selectedCase;
     console.log('this.selectedCase ', this.selectedCase);
+  }
+
+  returnToMainPanel(){
+    this.router.navigateToRoute('base');
   }
 }
