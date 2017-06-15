190d96be2fcbb7d6e242712e74ecdf87f88bdc94
AlanLeszczuk <alan.leszczuk@altimi.com>
2017-06-15 17:51:01 +0200

    finish kanban like table

--- src/views/employee/main-panel/main-panel.ts
+++ src/views/employee/main-panel/main-panel.ts
@@ -1,14 +1,54 @@
-import {Dragula} from 'aurelia-dragula';
 import {inject} from 'aurelia-framework';
 import {EmployeeTasksService} from '../../../services/employee-tasks-service';
+import {TasksService} from "../../../services/tasks-service";
 
-@inject(EmployeeTasksService)
+@inject(EmployeeTasksService, TasksService)
 export class MainPanel {
-  tasks;
+  statusChanging;
+  allTasks;
 
-  constructor(private employeeTasksService: EmployeeTasksService, public dragula: Dragula) {
-    employeeTasksService.getTasks()
-      .then(data => this.tasks = data);
+  tasksTodo;
+  tasksInProgress;
+  tasksDone;
+
+  constructor(private employeeTasksService: EmployeeTasksService, private tasksService: TasksService) {
+    this.statusChanging = false;
+    this.getTasks();
+  }
+
+  private filterTasksTodo(allTasks) {
+    this.tasksTodo = allTasks.filter(task => task.status === 'Assigned');
+  }
+
+  private filterTasksInProgress(allTasks) {
+    this.tasksInProgress = allTasks.filter(task => task.status === 'Pending');
+  }
+
+  private filterTasksDone(allTasks) {
+    this.tasksDone = allTasks.filter(task => task.status === 'Done');
+  }
+
+  updateTaskStatus(taskId, status) {
+    this.statusChanging = true;
+    this.tasksService.updateStatus(taskId, status)
+      .then(res => {
+        this.getTasks();
+      })
+      .catch(err => {
+        alert("Coś poszło nie tak, status: " + err);
+        this.statusChanging = false;
+      });
+  }
+
+  private getTasks() {
+    this.employeeTasksService.getTasks()
+      .then(data => {
+        this.allTasks = data;
+        this.filterTasksTodo(data);
+        this.filterTasksInProgress(data);
+        this.filterTasksDone(data);
+      })
+      .then(() => this.statusChanging = false);
   }
 
 }
